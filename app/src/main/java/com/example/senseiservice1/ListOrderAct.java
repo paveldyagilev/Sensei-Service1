e062303d364982b1244ede74312650dc0309eb55
paveldyagilev <paveldyagsem04@mail.ru>
2021-05-07 14:10:57 +0900

    third commit

--- app/src/main/java/com/example/senseiservice1/ListOrderAct.java
+++ app/src/main/java/com/example/senseiservice1/ListOrderAct.java
@@ -2,13 +2,30 @@ package com.example.senseiservice1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ListOrderAct extends AppCompatActivity {
+
+    private Button sidebar_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_order);
+
+        sidebar_btn = (Button) findViewById(R.id.sidebar_btn);
+        sidebar_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSidebarAct();
+            }
+        });
+    }
+    public void openSidebarAct(){
+        Intent i = new Intent(this, SidebarAct.class);
+        startActivity(i);
     }
 }
